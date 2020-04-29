2a7790b1428e15a96c45bb44e71bff183968812e
Shaon Majumder <smazoomder@gmail.com>
2020-04-29 10:21:54 +0600

    First release;

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name = 'AESClient',
     packages = ['AESClient'],
-    version = '0.0.0.0.1',
+    version = '0.0.0.0.2',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author = 'Shaon Majumder',
     author_email = 'smazoomder@gmail.com',
     url = 'https://github.com/ShaonMajumder/AESClient',
-    download_url = 'https://github.com/ShaonMajumder/AESClient/archive/0.0.0.0.1.tar.gz',
+    download_url = 'https://github.com/ShaonMajumder/AESClient/archive/0.0.0.0.2.tar.gz',
     keywords = ['AES', 'Encryption', 'Security', 'Shaon', 'Majummder'],
     classifiers = [],
     setup_requires=['wheel']
